6e7f8be34beb4cd99d1232ced4284477eff6e009
solenedel <solene98@student.ubc.ca>
2021-11-16 10:53:28 -0800

    define basic drawing functions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  const startDrawing = () => {
+
+  };
+
+  const finishDrawing = () => {
+
+  };
+
+  const draw = () => {
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <canvas
+      onMouseDown={startDrawing()}
+      onMouseUp={finishDrawing()}
+      onMouseMove={draw()}
+    />
   );
 }
 
