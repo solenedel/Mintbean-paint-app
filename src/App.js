54c43f33fe2e40468109e0b01312c66aa4b4a9d1
solenedel <solene98@student.ubc.ca>
2021-11-16 11:01:41 -0800

    style canvas and define context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-
+import React, { useRef, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  // information to conserve between re-renders
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.width.style = `${window.innerWidth}px`;
+    canvas.height.style = `${window.innerHeight}px`;
+    
+    const context = canvas.getContext('2d');
+    context.scale(2,2);
+    context.lineCap = 'round';
+    context.strokeStyle = 'black'; //NOTE: user will be able to choose this later
+    context.lineWith = 5; // NOTE: user will be able to choose this later
+    contextRef.current = context;
+  }, []);
 
   const startDrawing = () => {
 
@@ -17,9 +36,10 @@ function App() {
 
   return (
     <canvas
-      onMouseDown={startDrawing()}
-      onMouseUp={finishDrawing()}
-      onMouseMove={draw()}
+      onMouseDown={startDrawing}
+      onMouseUp={finishDrawing}
+      onMouseMove={draw}
+      ref={canvasRef}
     />
   );
 }
