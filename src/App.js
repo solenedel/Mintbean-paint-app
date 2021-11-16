8b78258b91029450b03c2b9ee8a4173cbb072882
solenedel <solene98@student.ubc.ca>
2021-11-16 12:18:22 -0800

    move code to Canvas component

--- src/App.js
+++ src/App.js
@@ -1,63 +1,11 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
+import Canvas from './components/Canvas';
 
 function App() {
 
-  // information to conserve between re-renders
-  const canvasRef = useRef(null);
-  const contextRef = useRef(null);
-
-  const [isDrawing, setIsDrawing] = useState(false);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    canvas.style.width = `${window.innerWidth}px`;
-    canvas.style.height = `${window.innerHeight}px`;
-    
-    const context = canvas.getContext('2d');
-    context.scale(1,1);
-    context.lineCap = 'round';
-    context.strokeStyle = 'black'; //NOTE: user will be able to choose this later
-    context.lineWith = 10; // NOTE: user will be able to choose this later
-    contextRef.current = context;
-  }, []);
-
-  // triggered when user presses down on the mouse
-  const startDrawing = ({ nativeEvent }) => {
-    const { offsetX, offsetY } = nativeEvent;
-    contextRef.current.beginPath();
-    contextRef.current.moveTo(offsetX, offsetY);
-    setIsDrawing(true);
-  };
-
-  const finishDrawing = () => {
-    contextRef.current.closePath();
-    setIsDrawing(false);
-  };
-
-  const draw = ({ nativeEvent }) => {
-    // only draw if the mouse is pressed down
-    // use this Word Clause format to decrease nesting
-    if (!isDrawing) {
-      return;
-    }
-
-    const { offsetX, offsetY } = nativeEvent;
-    contextRef.current.lineTo(offsetX, offsetY);
-    contextRef.current.stroke();
-
-  };
-
   return (
-    <canvas
-      style={{backgroundColor: 'pink'}}
-      onMouseDown={startDrawing}
-      onMouseUp={finishDrawing}
-      onMouseMove={draw}
-      ref={canvasRef}
-    />
-  );
+    <Canvas />
+  )
 }
 
 export default App;
--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from 'react';
 
 const Canvas = () => {
-  return ( 
+  // information to conserve between re-renders
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
     
-  );
-}
+    const context = canvas.getContext('2d');
+    context.scale(1,1);
+    context.lineCap = 'round';
+    context.strokeStyle = 'black'; //NOTE: user will be able to choose this later
+    context.lineWith = 10; // NOTE: user will be able to choose this later
+    contextRef.current = context;
+  }, []);
+
+  // triggered when user presses down on the mouse
+  const startDrawing = ({ nativeEvent }) => {
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    setIsDrawing(true);
+  };
+
+  const finishDrawing = () => {
+    contextRef.current.closePath();
+    setIsDrawing(false);
+  };
+
+  const draw = ({ nativeEvent }) => {
+    // only draw if the mouse is pressed down
+    // use this Word Clause format to decrease nesting
+    if (!isDrawing) {
+      return;
+    }
+
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+
+  };
+
+  return (
+    <canvas
+      style={{backgroundColor: 'pink', border: '10px solid blue'}}
+      onMouseDown={startDrawing}
+      onMouseUp={finishDrawing}
+      onMouseMove={draw}
+      ref={canvasRef}
+    />
+  )
+};
  
 export default Canvas;
