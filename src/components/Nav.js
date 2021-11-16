84a24520c2264e5b3dd7549cdfffbdf47e27da93
solenedel <solene98@student.ubc.ca>
2021-11-16 13:13:21 -0800

    create Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = ({ className }) => {
+  return ( 
+    <nav className={className}>
+      <div id="nav-flexbox">
+        <div id="logo">Café Finder</div>
+      </div>
+    </nav>
+   );
+}
+ 
+export default Nav;
