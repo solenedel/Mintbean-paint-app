1103a361c8338da33cbafc5edc50d9a4da9e88a9
solenedel <solene98@student.ubc.ca>
2021-11-19 19:49:56 -0800

    collapsible button styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,21 +1,28 @@
-import React from "react";
-import Collapsible from "./Collapsible";
+import React, {useState} from "react";
 // import {colorVars} from './styled/css-variables';
 
 export const Dashboard = (props) => {
 
-  const { className, changeCanvasColor } = props;
+  const { className, changeCanvasColor} = props;
+
+  
+  const[isOpenCanvas, setIsOpenCanvas] = useState(false);
+  const[isOpenBrush, setIsOpenBrush] = useState(false);
 
   return ( 
     <section id="dashboard" className={className}>
       <button id="undo"><span>Undo</span></button>
       <button id="clear"><span>Clear canvas</span></button>
-      <button id="brush-optn"><span>Brush options</span></button>
-      <Collapsible label="brush options"/>
 
-      <button id="canvas-optn"
-      onClick={() => changeCanvasColor()}><span>Canvas color</span></button>
-      <Collapsible label="canvas color"/>
+      <div className="collapsible" style={{color: 'white'}}>
+        <button className="toggle" id="canvas-optn" onClick={() => setIsOpenCanvas(!isOpenCanvas)}><span>Canvas color</span></button>
+        {isOpenCanvas && <div className="content">Canvas color options</div>} 
+      </div>
+
+      <div className="collapsible" style={{color: 'white'}}>
+        <button className="toggle" id="brush-optn" onClick={() => setIsOpenBrush(!isOpenBrush)}><span>Brush options</span></button>
+        {isOpenBrush && <div className="content">Brush options</div>} 
+      </div>
 
       <button id="save"><span>Save</span></button>
     </section>
