c34d44122f99e8f25598014a2a3822185b5ca3c1
solenedel <solene98@student.ubc.ca>
2021-11-21 17:33:35 -0800

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { StyledCanvas } from './components/styled/Canvas.style';
 import { GlobalStyles } from './components/styled/GlobalStyles.style';
 import { StyledNav } from './components/styled/Nav.style';
@@ -24,26 +25,39 @@ function App() {
   
 
   return (
-    <div className="app">
-      <GlobalStyles />
-      <StyledNav />
-      <div id="canvas-container">
-        <StyledCanvas
-          canvasColor={canvasColor}
-          setCanvasColor={(color) => {
-            setCanvasColor(color.hex);
-          }}
-          brushColor={brushColor}
-          setBrushColor={(color) => {
-            console.log('brush color.hex', color.hex);
-            setBrushColor(color.hex);
-          }}
-          brushSize={brushSize}
-          setBrushSize={setBrushSize}
-        />
+    <Router>
+      <div className="app">
+
+        <Switch>
+          <Route exact path="/">
+            <GlobalStyles />
+            <StyledNav />
+            <div id="canvas-container">
+              <StyledCanvas
+                canvasColor={canvasColor}
+                setCanvasColor={(color) => {
+                  setCanvasColor(color.hex);
+                }}
+                brushColor={brushColor}
+                setBrushColor={(color) => {
+                  console.log('brush color.hex', color.hex);
+                  setBrushColor(color.hex);
+                }}
+                brushSize={brushSize}
+                setBrushSize={setBrushSize}
+              />
+            </div>
+            <StyledFooter />
+          </Route>
+
+          <Route exact path="/landing-page">
+              <div>this is the landing page</div>
+          </Route>
+
+        </Switch>
       </div>
-      <StyledFooter />
-    </div>
+    </Router>
+    
   );
 };
 
