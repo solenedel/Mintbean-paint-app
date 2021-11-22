b5aa6341361040c272335b69b80e1e909518775e
solenedel <solene98@student.ubc.ca>
2021-11-21 18:32:13 -0800

    styling landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,10 +4,15 @@ const LandingPage = ({className}) => {
   return ( 
     <div id="landing-page" className={className}>
       <header>
-        <h4>Stuck in a zoom meeting?</h4>
-        <h1>try <span id="pintura">Pintura</span> for free!</h1>
+        <div id="title">Pintura</div>
+        <button><a href="/">try Pintura</a></button>
       </header>
-      
+
+      <div className="tagline">
+        <h4>Stuck in a zoom meeting</h4>
+        <h4>Need something to do?</h4>
+        <h1>try <span id="pintura">Pintura</span> for free!</h1>
+      </div>
     </div>
    );
 }
--- src/components/styled/LandingPage.style.js
+++ src/components/styled/LandingPage.style.js
@@ -5,9 +5,47 @@ import { colorVars, fonts } from './css-variables';
 export const StyledLandingPage = styled(LandingPage)`
 
 header {
-  margin-left: 40px; 
-  margin-top: 40px;
+  padding-left: 40px;
+  background-color:${colorVars.primaryLight};
+  height: 100px;
+  margin-bottom: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
+
+.tagline {
+  margin-left: 40px;
+}
+
+#title {
+  color: black;
+  font-size: 70px;
+  font-family: ${fonts.titleFont};
+}
+
+button {
+  height: 60px;
+  border: solid black 3px;
+  border-radius: 5px;
+  padding-left: 5px;
+  padding-right: 5px;
+  margin-right: 80px;
+  background-color: ${colorVars.linkHover};
+
+  &:hover {
+    background-color: #f8f097;
+  }
+}
+
+a, a:visited {
+  font-family: ${fonts.mainFont};
+  font-size: 25px;
+  font-weight: 500;
+  color: black;
+  text-decoration: none;
+}
+
 
 p, h1, h2, h3, h4, h5, h6 {
   color: ${colorVars.primaryLight}; 
@@ -16,11 +54,12 @@ p, h1, h2, h3, h4, h5, h6 {
 
 h1 {
   font-size: 100px;
+  margin-top: 40px;
 }
 
 h4 {
   font-family: ${fonts.mainFont};
-  font-size: 20px;
+  font-size: 25px;
   font-weight: 300;
 }
 
